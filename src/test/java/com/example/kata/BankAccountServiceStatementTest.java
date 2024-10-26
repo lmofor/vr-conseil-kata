c7be0c525122244f738d4e43a652422e138f739b
mamadou.diarra <mamadou.diarra@inexa-ci.com>
2024-10-26 12:47:39 +0000

    test: Mise en place de test pour la recupération du relevé des opération d' un compte bancaire

---
+++ src/test/java/com/example/kata/BankAccountServiceStatementTest.java
@@ -0,0 +1,59 @@
+package com.example.kata;
+
+import com.example.kata.dto.TransactionType;
+import com.example.kata.model.Account;
+import com.example.kata.model.Transaction;
+import com.example.kata.repository.BankAccountRepository;
+import com.example.kata.service.BankAccountService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * <p>Classe de test unitaire pour la récupération du rélévé des opérations d'un compte bancaire.</p>
+ *
+ * @author mamadou.diarra 2024-10-26
+ */
+public class BankAccountServiceStatementTest {
+
+    private BankAccountService bankAccountService;
+    private BankAccountRepository bankAccountRepository;
+
+    @BeforeEach
+    void setUp() {
+        this.bankAccountRepository = new BankAccountRepository();
+        this.bankAccountService = new BankAccountService(this.bankAccountRepository);
+    }
+
+    @Test
+    @DisplayName("Test de recupération de relevé vide !")
+    void testToGetVoidStatement() {
+        //Given
+
+        //When
+        List<Transaction> transactions = bankAccountService.transactions();
+
+        //Then
+        assertEquals(0, transactions.size());
+    }
+
+    @Test
+    @DisplayName("Test de recupération de relevé !")
+    void testToGetStatement() {
+        //Given
+        Account account = this.bankAccountRepository.getAccount();
+        Transaction transaction = new Transaction(LocalDateTime.now(), TransactionType.DEPOSIT, 1000, 1000);
+        account.getTransactions().add(transaction);
+
+        //When
+        List<Transaction> transactions = bankAccountService.transactions();
+
+        //Then
+        assertEquals(1, transactions.size());
+    }
+}
